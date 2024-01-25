58c110ee0893f729d44698543be90678b1c1b606
Catharina Trust <144069315+cgtrust@users.noreply.github.com>
2024-01-24 22:29:01 -0800

    resized images and added files fixed styling on mobile version

---
+++ scripts/australia.js
@@ -0,0 +1,7 @@
+// Navigation toggle
+const button = document.getElementById('menu-toggle');
+const links = document.getElementById('header-menu');
+
+button.addEventListener( 'click', function() {
+    links.classList.toggle( 'toggled' );
+});
